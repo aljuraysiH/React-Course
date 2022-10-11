8e264bbb83959f25c9c9ba8a9871629234348ec3
Hamad Aljuraysi <98861595+aljuraysiH@users.noreply.github.com>
2022-10-11 22:26:49 +0300

    Delete context directory

--- context/src/components/context/Context.jsx
+++
@@ -1,6 +0,0 @@
-import List from './List';
-
-const Context = () => {
-  return <List />;
-};
-export default Context;
--- context/src/components/propDrilling/Item.jsx
+++
@@ -1,9 +0,0 @@
-const Item = ({ name, id, removePerson }) => {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <button onClick={() => removePerson(id)}>Delete</button>
-    </div>
-  );
-};
-export default Item;
